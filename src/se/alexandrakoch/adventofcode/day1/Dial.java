80e770f897a1749bc12cdb6a9242136a7dc65825
Alexandra Koch <alexandra@alexandrakoch.se>
2025-12-04 19:47:00 +0100

    Initial commit + day1

---
+++ src/se/alexandrakoch/adventofcode/day1/Dial.java
@@ -0,0 +1,41 @@
+package se.alexandrakoch.adventofcode.day1;
+
+public class Dial {
+    private int dialPosition;
+
+    public Dial() {
+        dialPosition = 50;
+    }
+
+    public boolean turnDial(char direction, int steps) {
+        //Takes a char for direction and integer for number of steps to move
+        switch (direction) {
+            //Turn left = decrement value
+            case 'L':
+                dialPosition -= steps;
+                break;
+            //Turn right = increment value
+            case 'R':
+                dialPosition += steps;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid direction input");
+        }
+        //Loop around if value is under 0 or greater than 99
+        if (dialPosition < 0) {
+            int underrunTimes = (dialPosition * -1) / 100;
+            int remainder = ((dialPosition + (100 * underrunTimes)) * -1);
+            dialPosition = 100 - remainder;
+            if (dialPosition >= 100) {
+                dialPosition -= 100;
+            }
+        } else if (dialPosition >= 100) {
+            int overrunTimes = dialPosition / 100;
+            dialPosition -= overrunTimes * 100;
+            if (dialPosition >= 100) {
+                dialPosition -= 100;
+            }
+        }
+        return dialPosition == 0;
+    }
+}
---
+++ src/se/alexandrakoch/adventofcode/day1/Main.java
@@ -0,0 +1,23 @@
+import se.alexandrakoch.adventofcode.day1.Dial;
+
+void main() {
+    int password = 0;
+    Dial dial = new Dial();
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/day1.txt")))) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            char direction = line.charAt(0);
+            int value = Integer.parseInt(line.substring(1));
+            if (dial.turnDial(direction, value)) {
+                password++;
+            }
+        }
+        System.out.println("The password is: " + password);
+    } catch (IOException e) {
+        System.out.println("Error reading file");
+    } catch (NullPointerException e) {
+        System.out.println("File not found");
+    }
+
+
+}
